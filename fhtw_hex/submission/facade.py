0a06daf85f0051eefce595270b399eb78dcf2f06
sara-spreitzhofer <sara.spreitzhofer@tailored-apps.com>
2024-05-14 15:34:55 +0200

    initial commit

---
+++ fhtw_hex/submission/facade.py
@@ -0,0 +1,6 @@
+# stupid example
+def agent(board, action_set):
+    return action_set[0]
+
+# Here should be the necessary Python wrapper for your model, in the form of a callable agent, such as above.
+# Please make sure that the agent does actually work with the provided Hex module.
