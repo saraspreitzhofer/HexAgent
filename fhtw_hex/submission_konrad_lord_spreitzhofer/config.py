7ecad8043898cf39273f74933559bfbc4d086524
Tim Konrad <tim.konrad@gmx.at>
2024-05-24 01:26:26 +0200

    increase training time

--- fhtw_hex/submission_konrad_lord_spreitzhofer/config.py
+++ fhtw_hex/submission_konrad_lord_spreitzhofer/config.py
@@ -1,5 +1,5 @@
 BOARD_SIZE = 3
-EPOCHS = 3
-NUM_OF_GAMES_PER_EPOCH = 3
+EPOCHS = 100
+NUM_OF_GAMES_PER_EPOCH = 10
 # MODEL = 'fhtw_hex/submission_konrad_lord_spreitzhofer/models/final/best_hex_model.keras'  # for the final submission
 MODEL = 'fhtw_hex/submission_konrad_lord_spreitzhofer/models/2024-05-22-22-54-34/best_hex_model.keras'  # enter the folder to test
