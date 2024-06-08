8c49de818f008e077774c958613ead5de7aa5e06
EliasLo <146027019+EliasLo@users.noreply.github.com>
2024-06-08 22:05:43 +0200

    Update - add dropout layer to neural network - change legend in plots

--- fhtw_hex/submission_konrad_lord_spreitzhofer/facade.py
+++ fhtw_hex/submission_konrad_lord_spreitzhofer/facade.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from copy import deepcopy
 from fhtw_hex import hex_engine as engine
 from fhtw_hex.submission_konrad_lord_spreitzhofer import config
-
+#
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
